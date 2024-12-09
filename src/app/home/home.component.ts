031b17c40e058f2e7e0e07f398b5b1bf164e11b5
Rahyll <rahilreo@gmail.com>
2024-12-09 20:41:04 +0530

    Create a signal for saving courses fetch from courses services created to fetch data from backend through http and created computed signal based on the courses signal to filter and display advance and beginner course

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,61 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  signal,
+} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import {
+  toObservable,
+  toSignal,
+  outputToObservable,
+  outputFromObservable,
+} from '@angular/core/rxjs-interop';
+import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'home',
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  #courses = signal<Course[]>([]);
+  beginnerCourses = computed(() => {
+    const courses = this.#courses();
+    return courses.filter((course) => {
+      return course.category === 'BEGINNER';
+    });
+  });
+  advanceCourses = computed(() => {
+    const courses = this.#courses();
+    return courses.filter((course) => {
+      return course.category === 'ADVANCED';
+    });
+  });
+  coursesService = inject(CoursesServiceWithFetch);
+  constructor() {
+    this.loadAllCourses();
+    effect(() => {
+      console.log('Beginner Courses:::::::::>', this.beginnerCourses());
+      console.log('Advance Courses::::::::::>', this.advanceCourses());
+    });
+  }
+  async loadAllCourses() {
+    try {
+      const courses = await this.coursesService.loadAllCourses();
+      this.#courses.set(courses);
+    } catch (err) {
+      alert('Error loading courses');
+      console.error(err);
+    }
+  }
 }
--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -1,14 +1,16 @@
-import {Injectable} from "@angular/core";
-import {environment} from "../../environments/environment";
-import {Course} from "../models/course.model";
-
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Course } from '../models/course.model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class CoursesServiceWithFetch {
-
   env = environment;
 
-
+  async loadAllCourses(): Promise<Course[]> {
+    let response = await fetch(`${this.env.apiRoot}/courses`);
+    let payload = await response.json();
+    return payload.courses;
+  }
 }
