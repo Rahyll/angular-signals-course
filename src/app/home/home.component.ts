c4e15294dbe105d4cc1629e42360d311fec2da94
Rahyll <rahilreo@gmail.com>
2024-12-10 16:47:19 +0530

    added card component to the home component,use card component to list the courses received from the homecomponent signal as input,added edit delete and view button for each individual courses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -52,7 +52,7 @@ export class HomeComponent {
   async loadAllCourses() {
     try {
       const courses = await this.coursesService.loadAllCourses();
-      this.#courses.set(courses);
+      this.#courses.set(courses.sort(sortCoursesBySeqNo));
     } catch (err) {
       alert('Error loading courses');
       console.error(err);
