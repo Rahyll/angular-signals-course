b490725e6275f680560838434ab4d53773cccbc1
Rahyll <rahilreo@gmail.com>
2024-12-10 16:48:18 +0530

    create method to fetch,update,delete and and create course using fetch or http client

--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Course } from '../models/course.model';
+import { deleteCourse } from '../../../server/delete-course.route';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,33 @@ export class CoursesServiceWithFetch {
     let response = await fetch(`${this.env.apiRoot}/courses`);
     let payload = await response.json();
     return payload.courses;
+  }
+
+  async createCourse(course: Partial<Course>): Promise<Course> {
+    let response = await fetch(`${this.env.apiRoot}/courses`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(course),
+    });
+    return response.json();
+  }
+
+  async updateCourse(courseId: string, changes: Partial<Course>) {
+    let response = await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(changes),
+    });
+    return response.json();
+  }
+
+  async deleteCourse(courseId: string): Promise<void> {
+    let response = await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: 'DELETE',
+    });
   }
 }
